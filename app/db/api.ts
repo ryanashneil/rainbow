70eef13d2c41c539e4d9dcb8873c069cf1d923a8
ryanashneil <ryanashneil@gmail.com>
2020-01-14 17:19:17 +0800

    add get all user function

--- app/db/api.ts
+++ app/db/api.ts
@@ -1,9 +1,22 @@
 import firebase from ".";
 
 export const addUser = async (userId: string, newPerson: any) => {
-  const ref = `/${userId}`;
-  firebase
-    .database()
-    .ref(ref)
-    .push(newPerson);
+    const ref = `/${userId}`;
+    await firebase
+        .database()
+        .ref(ref)
+        .push(newPerson);
+};
+
+export const getAllUsers = async (userId: string) => {
+    const ref = `/${userId}`;
+    const users = [];
+    const data = await firebase
+        .database()
+        .ref(ref)
+        .once("value");
+    for (const key in data.val()) {
+        users.push(data.val()[key]);
+    }
+    return users;
 };
