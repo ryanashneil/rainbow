8c1a57cb44cf154094d0f10e798d296f3b4a4702
ryanashneil <ryanashneil@gmail.com>
2020-01-14 19:09:08 +0800

    add subtitle for nav

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,14 +1,15 @@
 import Segment from "components/Segment";
-import styled from "@emotion/styled";
 import { color } from "styles/tokens";
-import { Heading } from "@chakra-ui/core";
+import { Heading, Text } from "@chakra-ui/core";
 
 interface INavbar {
     title?: string;
+    subtitle?: string;
 }
 
 export default (props: INavbar): JSX.Element => (
     <Segment height={120} background={color.primary.v1}>
-        <Heading color="white">{props.title}</Heading>
+        {props.title && <Heading color="white">{props.title}</Heading>}
+        {props.subtitle && <Text color="white">{props.subtitle}</Text>}
     </Segment>
 );
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "components/Navbar";
 
 export default () => (
     <div>
-        <Navbar title="Profile" />
+        <Navbar title="Login" subtitle="Please login to continue" />
         <Segment marginTop="40px">
             <h1>Example H1 tag</h1>
         </Segment>
