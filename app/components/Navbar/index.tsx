3601af8dce45dfeb9d891d17d658ee64d97fba1a
ryanashneil <ryanashneil@gmail.com>
2020-01-14 13:45:20 +0800

    update navbar color

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,21 +1,15 @@
 import Segment from "components/Segment";
 import styled from '@emotion/styled';
 import { color } from "styles/tokens";
+import { Heading } from "@chakra-ui/core";
 
-export const NavContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid 
-    img {
-        width: 125px;
-    }
-`
 
-export default (): JSX.Element => (
-    <Segment width={1200} height={60} background={color.primary.v1}>
-        <NavContainer>
-        </NavContainer>
+interface INavbar {
+    title?: string;
+}
+
+export default (props: INavbar): JSX.Element => (
+    <Segment height={120} background={color.primary.v1}>
+        <Heading color='white'>{props.title}</Heading>
     </Segment>
 )
