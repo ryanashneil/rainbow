c798675fefab13e2ac0dd291fa296a0299acaa5e
ryanashneil <ryanashneil@gmail.com>
2020-01-14 11:05:47 +0800

    convert segment to use chakra box

--- app/components/Segment/index.tsx
+++ app/components/Segment/index.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import styled from '@emotion/styled'
-
-const SegmentWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background: ${(props: IProps) => props.background || 'transparent'};
-    height: ${(props: IProps) => props.height ? `${props.height}px` : 'auto'};
-    margin-top: ${(props: IProps) => props.marginTop ? `${props.marginTop}px` : '0'};
-    padding: 0 20px;
-`;
-
-const SegmentContainer = styled.div`
-    width: 100%;
-    max-width: ${(props: IProps) => props.width ? `${props.width}px` : '700px'};
-`;
+import { Box } from '@chakra-ui/core';
 
 interface IProps extends React.Props<{}> {
     background?: string;
     height?: number;
     width?: number;
-    marginTop?: number;
+    marginTop?: string;
 }
 
 export default (props: IProps): JSX.Element => (
-    <SegmentWrapper height={props.height} background={props.background} marginTop={props.marginTop}>
-        <SegmentContainer width={props.width}>{props.children}</SegmentContainer>
-    </SegmentWrapper>
+    <Box
+        width='100%'
+        display='flex'
+        flexDirection='row'
+        justifyContent='center'
+        alignItems='center'
+        height={props.height}
+        padding='0 20px'
+        background={props.background || 'transparent'}
+        marginTop={props.marginTop || 'auto'}>
+        <Box maxWidth={props.width || '700px'} width='100%'>
+            {props.children}
+        </Box>
+    </Box>
 );
