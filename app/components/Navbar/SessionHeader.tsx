9b760bb5c37377575d286a36bf4622d5c83b4b24
ryanashneil <ryanashneil@gmail.com>
2020-01-15 23:47:03 +0800

    add head meta-data

--- app/components/Navbar/SessionHeader.tsx
+++ app/components/Navbar/SessionHeader.tsx
@@ -5,7 +5,7 @@ import { Flex, Button } from "@chakra-ui/core";
 export default (): JSX.Element => {
     if (!isLoggedIn()) return <div />;
     return (
-        <Segment height={30} background="#252A2E">
+        <Segment height={30} background="#323233">
             <Flex direction="row-reverse">
                 <Button
                     variant="unstyled"
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import Theme from "styles/global";
+import Head from "next/head";
 
 export default ({ Component, pageProps }) => (
     <ThemeProvider>
+        <Head>
+            <title>Passport</title>
+            <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+            />
+        </Head>
         <CSSReset />
         <Theme />
         <Component {...pageProps} />
